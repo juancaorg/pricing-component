0b527762d2b0239001734a50025e281fc4d01133
Juan Carrillo <juan@juanca.org>
2022-11-24 15:53:07 -0600

    feat(script): change price cost accordingly using the toggle switch

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello! 👋");
+// Get all the price cost elements in an array.
+const priceCostArr = document.querySelectorAll(".main__card--price-cost");
+// Get the toggle switch checkbox.
+const toggleSwitch = document.getElementById("main__toggle--switch");
+
+toggleSwitch.addEventListener("change", () => {
+  // If the toggleSwitch is checked, show monthly prices.
+  // Otherwise, show annually prices (default).
+  if (toggleSwitch.checked) {
+    // Basic.
+    priceCostArr[0].textContent = "19.99";
+    // Professional.
+    priceCostArr[1].textContent = "24.99";
+    // Master.
+    priceCostArr[2].textContent = "39.99";
+  } else {
+    // Basic.
+    priceCostArr[0].textContent = "199.99";
+    // Professional.
+    priceCostArr[1].textContent = "249.99";
+    // Master.
+    priceCostArr[2].textContent = "399.99";
+  }
+});
